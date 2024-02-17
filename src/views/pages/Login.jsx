8cc998b21be241e6f13f6f653b65e8da5eaad980
yogiedigital <yogiedigital@gmail.com>
2024-02-17 21:45:43 +0700

    last add

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -1,35 +1,54 @@
-// import React from 'react';
+import React from 'react';
 // import { useDispatch } from 'react-redux';
 // import { login } from '../actions/authActions';
+// import { useNavigate } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
 
 const Login = () => {
   // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
     // dispatch(login());
     console.log("Login Press")
-
-    fetch('https://dummyjson.com/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
+    event.preventDefault();
+    ipcRenderer.send('login', { username: 'usernameMu', password: 'passwordMu' });
+    // fetch('https://dummyjson.com/auth/login', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({
         
-        username: 'kminchelle',
-        password: '0lelplR',
-        // expiresInMins: 60, // optional
-      })
-    })
-    .then(res => res.json())
-    .then(console.log);
+    //     username: 'kminchelle',
+    //     password: '0lelplR',
+    //     // expiresInMins: 60, // optional
+    //   })
+    // })
+    // .then(res => res.json())
+    // .then(console.log);
 
   };
 
+  // const handleAnother = () => {
+  //   navigate('/halo');
+  // }
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <form onSubmit={handleLogin}>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>    
   );
+
+  // return (
+  //   <div>
+  //     <h1>Login Page</h1>
+  //     <button onClick={handleLogin}>Login</button>
+  //     <button onClick={handleAnother}>Another</button>
+  //   </div>
+  // );
 };
 
 export default Login;
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: 'localhost',
   plugins: [react()],
   build: {
-    outDir: 'build'
+    // outDir: '../test-dss-electron/build',
+    // assetsDir: './'
+    // assetsDir: 'static',
   }
 })
